25cc5ed3eac0e71dda615472839090738e9dba85
hjob-1 <hjob12tadele@gmail.com>
2024-04-21 19:05:40 -0400

    first commit

--- obj/Debug/net8.0/Q1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Q1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Q1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d83d0660d7c18f93a1e28c984365f7fa91ac4ab4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c06135d6e8767ed252c614432c4678030c5a26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Q1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Q1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
